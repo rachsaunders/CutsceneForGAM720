63d60b37fa9bb5de199ed5fc4cb3284159303a5c
Rachel Saunders <rachelsaundersBA@gmail.com>
2020-04-24 19:36:40 +0100

    Example/Rough sketches have been set, segues linked. In early stages.

--- CutsceneForGAM720/ViewController.swift
+++ CutsceneForGAM720/ViewController.swift
@@ -5,8 +5,14 @@
 //  Created by Rachel Saunders on 24/04/2020.
 //  Copyright © 2020 Rachel Saunders. All rights reserved.
 //
+// THIS APP IS SOLELY FOR THE PURPOSE OF ASSIGNMENT 2 FOR GAM720 MODULE AS PART OF THE MASTERS DEGREE FOR CREATIVE APP DEVELOPMENT. ALL PROGRAMMING, NOTES AND ARTWORK BY RACHEL SAUNDERS. THIS IS A TEST TO SEE HOW 2D PLATFORMING WORKS. COPYRIGHT RACHEL SAUNDERS.
 
 import UIKit
+
+// Images are scaled to fill as they are examples and not final images.
+// Images all have image view contraints of 0 for each edge
+// Tested and fully functional on an iPhone 8
+// Removed animation from Segue properties
 
 class ViewController: UIViewController {
 
