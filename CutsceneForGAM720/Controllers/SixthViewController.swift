31a71642cad92c17ffd64235963cfdc0dceaf411
Rachel Saunders <rachelsaundersBA@gmail.com>
2020-04-28 14:53:36 +0100

    Added swift files for each View Controller. Ready to experiment with sound input. Needs AVFoundation framework adding. Also organised files for easier access.

---
+++ CutsceneForGAM720/Controllers/SixthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SixthViewController.swift
+//  CutsceneForGAM720
+//
+//  Created by Rachel Saunders on 28/04/2020.
+//  Copyright © 2020 Rachel Saunders. All rights reserved.
+//
+
+import UIKit
+
+class SixthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CutsceneForGAM720/Controllers/ViewController.swift
+++ CutsceneForGAM720/Controllers/ViewController.swift
@@ -6,13 +6,16 @@
 //  Copyright © 2020 Rachel Saunders. All rights reserved.
 //
 // THIS APP IS SOLELY FOR THE PURPOSE OF ASSIGNMENT 2 FOR GAM720 MODULE AS PART OF THE MASTERS DEGREE FOR CREATIVE APP DEVELOPMENT. ALL PROGRAMMING, NOTES AND ARTWORK BY RACHEL SAUNDERS. THIS IS A TEST TO SEE HOW 2D PLATFORMING WORKS. COPYRIGHT RACHEL SAUNDERS.
-
+//
+//
 import UIKit
 
 // Images are scaled to fill as they are examples and not final images.
 // Images all have image view contraints of 0 for each edge
 // Tested and fully functional on an iPhone 8
 // Removed animation from Segue properties
+
+// Current Plan - Add files for all the view controllers, link up, and add the AVFoundation framework for sounds. Sounds will have to be background for each controller and not loop/repeat unless nessessary.
 
 class ViewController: UIViewController {
 
@@ -22,5 +25,8 @@ class ViewController: UIViewController {
     }
 
 
+
 }
+
+
 
